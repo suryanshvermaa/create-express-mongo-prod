274b654791c353690c1d4e8e7ead3d7e2fd553bd
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-07-12 16:26:21 +0530

    updated bin/index.js added all implemetations

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 const prompts = require("prompts");
 const { projectPrompt, packageManagerPrompt, languagePrompt, featuresPrompt, ormsPrompt } = require("../prompts/index.js");
 const values=require("../state/values.js");
+const { cloneRepo, renameProject, packageNameInpackageJson, installDependencies } = require("../functions/index.js");
+const {getRepoName,getBranchName}=require("../mapping/index.js");
 
 (async () => {
   const projectName = await prompts(projectPrompt);
@@ -13,71 +15,16 @@ const values=require("../state/values.js");
 
   values.projectName = projectName.projectName;
   values.packageManager = packageManager.packageManager;
+  values.language = language;
   values.projectName = response.projectName;
   values.databaseORM = orm.orm;
   values.features=[...features];
   
-  if(language.language=="typescript"){
-    const orm = await prompts({
-      type: "select",
-      name: "orm",
-      message: "You are using in this project?(ORM)",
-      choices:[
-        { title: "mongoose with mongodb", value: 0 },
-        { title: "postgres with prima", value: 1 }
-      ],
-      initial: 0,
-    });
-    if(orm.orm==0){
-      const graphQl = await prompts({
-        type: "select",
-        name: "graphql",
-        message: "Are you using graphQl in this project?",
-        choices:[
-          { title: "Yes", value: true },
-          { title: "No", value: false }
-        ],
-        initial: 0,
-      });
-      if(graphQl.graphql) execSync(`git clone --branch graphqlWithMongo --single-branch https://github.com/suryanshvermaa/nodeMongoTsTemplate.git && mv nodeMongoTsTemplate ${projectDir}`);
-      else await degit(tsRepo).clone(projectDir);
-    }else{
-      execSync(`git clone --branch prisma --single-branch https://github.com/suryanshvermaa/nodeMongoTsTemplate.git && mv nodeMongoTsTemplate ${projectDir}`);
-    }
-  }
-  else if(language.language=="javascript"){
-    await degit(jsRepo).clone(projectDir);
-  }else{
-    console.log("error in creating template");
-  }
-  console.log(`\n📦 Cloning starter template into "${projectDir}"...\n`);
-  // --- Modify package.json ---
-  const pkgPath = path.join(projectDir, "package.json");
-  const pkgJson = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
-
-  pkgJson.name = projectDir;
-
-  fs.writeFileSync(pkgPath, JSON.stringify(pkgJson, null, 2));
-  console.log(`\n📥 Installing dependencies...\n`);
-  const packageManagerCommands = {
-    npm: "npm install",
-    yarn: "yarn install",
-    pnpm: "pnpm install"
-  };
-  if(packageManagerChoice === "yarn") {
-    execSync(`cd ${projectDir} && yarn install`, { stdio: "inherit" });
-    console.log(`\n✅ Done!`);
-    console.log(`\nTo start:\ncd ${projectDir}\n  yarn dev\n`);
-    
-  }
-  else if(packageManagerChoice === "pnpm") {
-    execSync(`cd ${projectDir} && pnpm install`, { stdio: "inherit" });
-    console.log(`\n✅ Done!`);
-    console.log(`\nTo start:\ncd ${projectDir}\n pnpm dev\n`);
-  }
-  else{
-    execSync(`cd ${projectDir} && npm install`, { stdio: "inherit" });
-    console.log(`\n✅ Done!`);
-    console.log(`\nTo start:\ncd ${projectDir}\n npm run dev\n`);
-  }
+  // clone repo
+  const repo=getRepoName();
+  const branch=getBranchName();
+  await cloneRepo(repo,branch);
+  await renameProject(values.projectName, repo); 
+  await packageNameInpackageJson(projectName);
+  await installDependencies(values.projectName,values.packageManager);
 })();
