3f586f086931d340623050ce25feafb2cf59b4c7
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-07-12 13:39:19 +0530

    added repo data and branches

---
+++ repos/index.js
@@ -0,0 +1,39 @@
+const repos=[
+    {
+        id: 1,
+        language: "Typescript",
+        repo:"https://github.com/suryanshvermaa/nodeMongoTsTemplate.git",
+        databases:[
+            {
+                name: "MONGO",
+                branch:{
+                    EXPRESS:"main",
+                    AWS_S3:"express-awsS3",
+                    graphQL:"graphqlWithMongo",
+                    graphQL_AWS_S3:"express-mongoose-graphql-awsS3",
+                }
+            },
+            {
+                name:"POSTGRES",
+                branch:{
+                    EXPRESS:"prisma",
+                    AWS_S3:"express-prisma-awsS3"
+                }
+            }
+        ]
+    },
+
+    {
+        id: 2,
+        language: "Javascript",
+        repo: "https://github.com/suryanshvermaa/nodeMongoJsTemplate.git",
+        databases:[
+            {
+                name: "MONGO",
+                branch: {
+                    EXPRESS: "main"
+                }
+            }
+        ]
+    }
+]
